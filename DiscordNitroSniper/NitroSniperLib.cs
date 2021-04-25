00903a68b654c055108591755ad2e96ffbc9c7e0
Obscurely <59087558+Obscurely@users.noreply.github.com>
2021-04-25 10:00:08 +0300

    Add the solution instead of the 2 .cs files

---
+++ DiscordNitroSniper/NitroSniperLib.cs
@@ -0,0 +1,159 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+
+namespace DiscordNitroSniper
+{
+    class NitroSniperLib
+    {
+        private const string _proxiesUrl = "https://api.proxyscrape.com/v2/?request=getproxies&protocol=http&timeout=5000&country=all&ssl=all&anonymity=all&simplified=true";
+        private const string _codeCheckUrlFirst = "https://discord.com/api/v8/entitlements/gift-codes/";
+        private const string _codeCheckUrlLast = "?with_application=false&with_subscription_plan=true";
+        private const string _codeUrlFirst = "https://discord.gift/";
+        private string _proxiesFile = "proxies.txt";
+
+        public static string ProxiesUrl
+        {
+            get
+            {
+                return _proxiesUrl;
+            }
+        }
+
+        public static string CodeCheckUrlFirst
+        {
+            get
+            {
+                return _codeCheckUrlFirst;
+            }
+        }
+
+        public static string CodeCheckUrlLast
+        {
+            get
+            {
+                return _codeCheckUrlLast;
+            }
+        }
+
+        public string ProxiesFile
+        {
+            get
+            {
+                return _proxiesFile;
+            }
+            set
+            {
+                _proxiesFile = value;
+            }
+        }
+
+        public static string CodeUrlFirst
+        {
+            get
+            {
+                return _codeUrlFirst;
+            }
+        }
+
+        public async Task DownloadProxies()
+        {
+            using WebClient client = new();
+            await client.DownloadFileTaskAsync(new Uri(ProxiesUrl), ProxiesFile);
+        }
+
+        public async Task<string> PickProxy()
+        {
+            string proxyString = await File.ReadAllTextAsync(ProxiesFile);
+            List<string> proxies = new();
+            proxies.AddRange(proxyString.Split("\n"));
+            Random random = new();
+            string picked_proxy = proxies[random.Next(proxies.Count)];
+
+            if (picked_proxy.Equals(""))
+                return "no proxies left";
+
+            proxies.Remove(picked_proxy);
+            string remainedProxies = string.Join("\n", proxies);
+            await File.WriteAllTextAsync(ProxiesFile, remainedProxies);
+            return picked_proxy;
+        }
+
+        public string GetRandomNitroCode()
+        {
+            const string chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+            Random random = new();
+
+            string nitroCode = "";
+            for (int i = 0; i < 16; i++)
+            {
+                nitroCode += chars[random.Next(62)];
+            }
+            return nitroCode;
+        }
+
+        public async Task<string> CheckNitroCodeWithProxy(string giftCode, string proxy)
+        {
+            HttpClientHandler clientHandler = new()
+            {
+                AllowAutoRedirect = true,
+                AutomaticDecompression = DecompressionMethods.Deflate | DecompressionMethods.GZip,
+                Proxy = new WebProxy(new Uri($"http://{proxy}"))
+            };
+
+            HttpResponseMessage response;
+            using (HttpClient client = new(clientHandler))
+            {
+                client.Timeout = TimeSpan.FromSeconds(3);
+                response = await client.GetAsync(CodeCheckUrlFirst + giftCode + CodeCheckUrlLast);
+            }
+            HttpStatusCode rateLimitedCode = (HttpStatusCode)429;
+            HttpStatusCode successCode = (HttpStatusCode)200;
+            HttpStatusCode invalidCode = (HttpStatusCode)404;
+
+            if (response.StatusCode.Equals(successCode))
+            {
+                return CodeUrlFirst + giftCode;
+            }
+            else if (response.StatusCode.Equals(rateLimitedCode))
+            {
+                return "rate limited";
+            }
+            else if (response.StatusCode.Equals(invalidCode))
+            {
+                return "invalid gift";
+            }    
+            else
+            {
+                return "error";
+            }
+        }
+
+        public async Task<string> CheckNitroCodeWithoutProxy(string giftCode)
+        {
+            HttpResponseMessage response;
+            using (HttpClient client = new())
+            {
+                response = await client.GetAsync(CodeCheckUrlFirst + giftCode + CodeCheckUrlLast);
+            }
+
+            if (response.StatusCode.Equals(200))
+            {
+                return CodeUrlFirst + giftCode;
+            }
+            else
+            {
+                return "invalid gift";
+            }
+        }
+
+        public async Task WriteCodeToFile(string giftCode)
+        {
+            await File.AppendAllTextAsync("working_codes.txt", giftCode + "\n");
+        }
+    }
+}
---
+++ DiscordNitroSniper/Program.cs
@@ -0,0 +1,123 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace DiscordNitroSniper
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("Every working code will be printed to the console and outputed to working_gift_codes_txt!" +
+                "\nThe program uses proxies in order to avoid Rate Limit. The use and changing of proxies is done in the background." +
+                "\nRunning task... (Press any key to quit!)");
+
+            Thread checkRandomCodeThread1 = new(new ThreadStart(() => CheckRandomCode(1)));
+            Thread checkRandomCodeThread2 = new(new ThreadStart(() => CheckRandomCode(2)));
+            Thread checkRandomCodeThread3 = new(new ThreadStart(() => CheckRandomCode(3)));
+            Thread checkRandomCodeThread4 = new(new ThreadStart(() => CheckRandomCode(4)));
+            Thread checkRandomCodeThread5 = new(new ThreadStart(() => CheckRandomCode(5)));
+            Thread checkRandomCodeThread6 = new(new ThreadStart(() => CheckRandomCode(6)));
+            Thread checkRandomCodeThread7 = new(new ThreadStart(() => CheckRandomCode(7)));
+            Thread checkRandomCodeThread8 = new(new ThreadStart(() => CheckRandomCode(8)));
+            Thread checkRandomCodeThread9 = new(new ThreadStart(() => CheckRandomCode(9)));
+            Thread checkRandomCodeThread10 = new(new ThreadStart(() => CheckRandomCode(10)));
+            Thread checkRandomCodeThread11 = new(new ThreadStart(() => CheckRandomCode(11)));
+            Thread checkRandomCodeThread12 = new(new ThreadStart(() => CheckRandomCode(12)));
+            Thread checkRandomCodeThread13 = new(new ThreadStart(() => CheckRandomCode(13)));
+            Thread checkRandomCodeThread14 = new(new ThreadStart(() => CheckRandomCode(14)));
+            Thread checkRandomCodeThread15 = new(new ThreadStart(() => CheckRandomCode(15)));
+            Thread checkRandomCodeThread16 = new(new ThreadStart(() => CheckRandomCode(16)));
+            checkRandomCodeThread1.Start();
+            checkRandomCodeThread2.Start();
+            checkRandomCodeThread3.Start();
+            checkRandomCodeThread4.Start();
+            checkRandomCodeThread5.Start();
+            checkRandomCodeThread6.Start();
+            checkRandomCodeThread7.Start();
+            checkRandomCodeThread8.Start();
+            checkRandomCodeThread9.Start();
+            checkRandomCodeThread10.Start();
+            checkRandomCodeThread11.Start();
+            checkRandomCodeThread12.Start();
+            checkRandomCodeThread13.Start();
+            checkRandomCodeThread14.Start();
+            checkRandomCodeThread15.Start();
+            checkRandomCodeThread16.Start();
+            Console.ReadKey();
+        }
+
+        static async Task CheckRandomCode(int threadNumber)
+        {
+            NitroSniperLib nitro = new();
+            nitro.ProxiesFile = $"proxies_thread{threadNumber}.txt";
+            await nitro.DownloadProxies();
+            string proxy = await nitro.PickProxy();
+
+            string giftCode = nitro.GetRandomNitroCode();
+            while (true)
+            {
+                try
+                {
+                    string response = await nitro.CheckNitroCodeWithProxy(giftCode, proxy);
+                    if (response.Contains(NitroSniperLib.CodeUrlFirst))
+                    {
+                        Console.WriteLine($"Working code: {giftCode}\nWriting it to the file...");
+                        await nitro.WriteCodeToFile(response);
+                    }
+                    else if (response.Equals("rate limited"))
+                    {
+                        proxy = await nitro.PickProxy();
+                        if (proxy.Equals("no proxies left"))
+                        {
+                            await nitro.DownloadProxies();
+                            proxy = await nitro.PickProxy();
+                        }
+                    }
+                    else if (response.Equals("invalid gift"))
+                    {
+                        Console.WriteLine($"Invalid gift code! ({giftCode})");
+                    }
+                    else
+                    {
+                        proxy = await nitro.PickProxy();
+                        if (proxy.Equals("no proxies left"))
+                        {
+                            await nitro.DownloadProxies();
+                            proxy = await nitro.PickProxy();
+                        }
+                    }
+
+                    giftCode = nitro.GetRandomNitroCode();
+                }
+                catch (TaskCanceledException)
+                {
+                    proxy = await nitro.PickProxy();
+                    if (proxy.Equals("no proxies left"))
+                    {
+                        await nitro.DownloadProxies();
+                        proxy = await nitro.PickProxy();
+                    }
+                }
+                catch (System.Net.Http.HttpRequestException)
+                {
+                    proxy = await nitro.PickProxy();
+                    if (proxy.Equals("no proxies left"))
+                    {
+                        await nitro.DownloadProxies();
+                        proxy = await nitro.PickProxy();
+                    }
+                }
+                catch (UriFormatException)
+                {
+                    proxy = await nitro.PickProxy();
+                    if (proxy.Equals("no proxies left"))
+                    {
+                        await nitro.DownloadProxies();
+                        proxy = await nitro.PickProxy();
+                    }
+                }
+            }
+        }
+    }
+}
